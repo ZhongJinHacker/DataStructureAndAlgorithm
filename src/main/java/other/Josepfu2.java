a189e909ed37b02358703540c7e423f66fc1bd06
ZhongJinHacker <jiangzhongjin@hotmail.com>
2020-04-02 11:25:38 +0800

    todo

---
+++ src/main/java/other/Josepfu2.java
@@ -0,0 +1,15 @@
+package other;
+
+/**
+ * @author gradyjiang
+ * @title: Josepfu2 使用双向链表算法
+ * @projectName datastructure-and-algorithm
+ * @description: 约瑟夫问题算法
+ * n 个人围成一圈，从第一个人开始报数,数到 m 的人出列，
+ * 再由下一个人重新从 1 开始报数，数到 m 的人再出圈，依次类推，直到所有的人都出圈，请输出依次出圈人的编号。
+ * 输入两个整数 n, m。
+ * 输出一行 n 个整数，按顺序输出每个出圈人的编号
+ * @date 2020/2/28
+ */
+public class Josepfu2 {
+}
